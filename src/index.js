0b94afd2adcdea320b83bdb5d1453e3e7820421c
simo099 <simonesimoneb@libero.it>
2023-01-19 12:10:54 +0100

    reorganize and basic-auth

--- src/index.js
+++ src/index.js
@@ -7,28 +7,41 @@ const port = 3000;
 
 const db = pgp(process.env["DB_URL"]);
 
+const basicAuth = require('express-basic-auth')
+
 app.use(express.json());
 
+app.use(basicAuth({
+    users: { 'foo': 'bar' },
+    unauthorizedResponse: getUnauthorizedResponse
+}))
+
+function getUnauthorizedResponse(req) {
+    return req.auth
+        ? ('Unauthorized: credentials rejected')
+        : 'No credentials provided'
+}
+ 
+async function validation(req, res){
+    if((!req.body.first_name)||(!req.body.last_name)||(!req.body.address)){
+        return res.status(400).json({status: 400, message: "Bad Request", validation: false});
+    }
+    else{
+        res.json({validation: true});
+        await db.none("INSERT INTO users(first_name, last_name, address) VALUES (${first_name},${last_name},${address})",
+        req.body
+        )
+    }
+}
+
 app.get('/users', async (req, res) => {
-    const data = await db.query("SELECT * FROM users")
-    res.send(data);
+    const data = await db.query("SELECT * FROM users");
+    res.send(data)
 })
 
-app.put('/users', async (req, res) => {
-    if(!req.body.first_name){
-        return res.status(400).json({status: 400, message: "Bad Request"});
-    }
-    if(!req.body.last_name){
-        return res.status(400).json({status: 400, message: "Bad Request"});
-    }
-    if(!req.body.address){
-        return res.status(400).json({status: 400, message: "Bad Request"});
-    }
-    await db.none("INSERT INTO users(first_name, last_name, address) VALUES (${first_name},${last_name},${address})",
-        req.body
-    )
+app.put('/users', (req, res) => {
+    validation(req, res);
     res.send()
-    
 })
 
 app.listen(port, () => {
